2e4cc637fd8ec820c83771c7b504fa5bd045b3ee
CHEN YANG <chen.yang@utah.edu>
2016-12-02 12:52:17 -0700

    Update TeamChart.js

--- TeamChart.js
+++ TeamChart.js
@@ -114,7 +114,7 @@ TeamChart.prototype.update = function(value){
                 return j==i;
             })
                 .attr("x",function (d,j) {
-                    return svgWidth / 20 * i + 7.5;
+                    return svgWidth / 20 * i;
                 })
                 .attr("y", 20)
                 .classed("selectTeam", true);
